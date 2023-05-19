fe25ee1c0fce0506c51191ca6c6842d20f98833a
ItsCacia <caciaaicac@gmail.com>
2023-05-19 08:40:27 +0000

    boh

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Prodotti</title>
+	<link rel="stylesheet" href="styles.css">
+	<script src="script.js"></script>
+</head>
+<body>
+    <button id="login-button" onclick="openLoginWindow()">Login</button>
+  
+    <div class="container">
+        <input type="text" id="search-bar" placeholder="Search">
+    
+        <table id="product-table">
+            <?php include('index.php'); ?>
+        </table>
+    </div>
+</body>
+</html>

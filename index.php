f8570390078a7c02be0c23e4d51c97e38feda64f
ItsCacia <caciaaicac@gmail.com>
2023-05-22 11:58:43 +0000

    ora va

--- index.php
+++ index.php
@@ -6,30 +6,36 @@
 	<script src="script.js"></script>
 </head>
 <body>
-    <button id="login-button" onclick="openLoginWindow()">Login</button>
 
-    <div class="container">
-	<form action="index.php" method="get">
-		<input type="text" id="search-bar" placeholder="Search" name="search">
-	</form>
+    <header>    
+        <h2>Outlet spaziale #42</h2>
+        <button id="login-button" onclick="openLoginWindow()">Login</button>
+    </header>
+
+    <div id="container">
+        <div id="search-div">
+	        <form action="index.php" method="get">
+	        	<input type="text" id="search-bar" placeholder="Search" name="search">
+	        </form>
+        </div>
 
         <table id="product-table">
             <?php
-		ini_set('display_errors', 1);
-		ini_set('display_startup_errors', 1);
-		error_reporting(E_ALL);
+		        ini_set('display_errors', 1);
+		        ini_set('display_startup_errors', 1);
+		        error_reporting(E_ALL);
 
-		// Creo la connessione col database
+		        // Creo la connessione col database
                 $mysqli = new mysqli('localhost', 'guest', 'guest');
 
-		$mysqli->select_db("Esercitazione");
+		        $mysqli->select_db("Esercitazione");
 
-		if(isset($_GET['search'])) {
-			$search = $_GET['search'];
-			$result = $mysqli->query("SELECT * FROM prodotto WHERE nome LIKE '%$search%'", MYSQLI_USE_RESULT);
-		} else {
-                	$result = $mysqli->query("SELECT * FROM prodotto", MYSQLI_USE_RESULT);
-		}
+		        if(isset($_GET['search'])) {
+		        	$search = $_GET['search'];
+		        	$result = $mysqli->query("SELECT * FROM prodotto WHERE nome LIKE '%$search%'", MYSQLI_USE_RESULT);
+		        } else {
+                    $result = $mysqli->query("SELECT * FROM prodotto", MYSQLI_USE_RESULT);
+		        }
 
                 $count = 0;
                 while ($row = mysqli_fetch_assoc($result)) {
@@ -41,8 +47,8 @@
                     echo "<div class='product-item'>";
                     echo "<h3>" . $row['nome'] . "</h3>";
                     echo "<p>" . $row['descrizione'] . "</p>";
-                    echo "<p>" . $row['prezzo'] . "</p>";
-                    echo "<p>" . $row['quantita'] . "</p>";
+                    echo "<p>" . "Prezzo: " . $row['prezzo'] . "€" . "</p>";
+                    echo "<p>" . "Rimasti: " . $row['quantita'] . "</p>";
                     echo "</div>";
                     echo "</td>";
 
